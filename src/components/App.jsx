915191c495041e5c961203c12ec021744759d838
Eligio Quijada <eligio.eq@gmail.com>
2022-03-28 13:48:39 -0500

    v2 started

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,6 @@ import Footer from './Footer';
 import Header from './Header';
 import Note from './Note';
 
-function App(){
-    return(<div><Header/><Note/><Footer/></div>);
-}
+const App = () =>(<div><Header/><Note/><Footer/></div>);
 
 export default App;
