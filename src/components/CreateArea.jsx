e6806e8d60064a397ad2a72d26f6b9902aa2aa0c
Eligio <eligio.eq@gmail.com>
2022-04-07 09:40:51 -0500

    material ui added

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,38 +1,65 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import { Zoom } from "@mui/material";
 
-function CreateArea(props){
 
-    const [note, setNote] = useState({
-        title: "",
-        content: ""
+function CreateArea(props) {
+
+  const [isExpanded, setExpanded] = useState(false);
+
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
     });
+  }
 
-    function handleChange(event){
-        const {name, value} = event.target;
-        setNote(prevNote => {
-            return{
-                ...prevNote,
-                [name]:value
-            }
-        });
-    }
-    
-    function createNote(event){
-        props.onAdd(note);
-        event.preventDefault();
-        setNote({
-            title: "",
-            content: ""
-        });
-    }
+  function createNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
 
-    return(
-        <form>
-            <input value={note.title} onChange={handleChange} name="title" placeholder="Title"></input>
-            <textarea value={note.content} onChange={handleChange} name="content" placeholder="Take a note..." rows="3" />
-            <button onClick={createNote}>Add</button>
-        </form>
-    )
+  const expanded = () => setExpanded(true);
+
+  return (
+    <div>
+      <form className="create-note">
+      {isExpanded ? <input
+          value={note.title}
+          onChange={handleChange}
+          name="title"
+          placeholder="Title"
+        /> : null}
+
+        <textarea
+          onClick={expanded}
+          value={note.content}
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows={isExpanded ? 3:1}
+        />
+        <Zoom in={true}>
+          <Fab onClick={createNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div>
+  );
 }
 
 export default CreateArea;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import BoltIcon from '@mui/icons-material/Bolt';
 
-const Header = () => <header><h1>Keeper App</h1></header>;
+const Header = () => <header><h1><BoltIcon/>Keeper App</h1></header>;
 
 export default Header;
 
